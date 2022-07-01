61ed9845950d7967d45eb41031418ead29a8b940
Hugodesousa <hugodesousa.16@gmail.com>
2022-07-01 15:33:45 -0300

    requisito 1

---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <h1>ProfileEdit</h1>
+      </div>
+    );
+  }
+}
+export default ProfileEdit;
