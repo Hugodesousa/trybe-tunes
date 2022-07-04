b149e152b4385e15c6a817951c891ee08b964483
Hugodesousa <hugodesousa.16@gmail.com>
2022-07-04 12:02:07 -0300

    requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,57 @@ import React, { Component } from 'react';
 import Header from '../Components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      valueSearch: '',
+      stateButton: true,
+    };
+  }
+
+  desableButton = () => true
+
+  input = ({ target }) => {
+    this.setState({
+      valueSearch: target.value,
+    });
+    if (target.value.length >= 2) {
+      return this.setState({
+        stateButton: false,
+      });
+    }
+    return this.setState({
+      stateButton: true,
+    });
+  }
+
   render() {
+    const { valueSearch, stateButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <form>
+          <label htmlFor="inputsearch">
+            <input
+              id="inputsearch"
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.input }
+              value={ valueSearch }
+            />
+          </label>
+          <button
+            type="button"
+            id="searchButton"
+            data-testid="search-artist-button"
+            disabled={ stateButton }
+          >
+            Pesquisar
+          </button>
+
+        </form>
+
       </div>
     );
   }
